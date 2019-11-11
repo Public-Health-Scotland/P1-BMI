12eec266f1c29946a5128d1666bea522ead00364
michael <michael.nisbett@nhs.net>
2019-11-11 15:45:53 +0000

    replace each population estimate filter with a function.

--- P1_BMI_Stats.R
+++ P1_BMI_Stats.R
@@ -557,27 +557,7 @@ hb_pop_estimates <- readRDS(paste0(
   lookup_folder, "/Unicode/Populations/Estimates/HB2019_pop_est_1981_2018.rds")) %>%
   rename(year = Year, age = Age, pop = Pop)
 
-hb_pop_estimates <- hb_pop_estimates %>% 
-  filter(age == 5) %>%
-  filter(year >= 2001 & year <=2018) %>% 
-  mutate(schlyr_exam = case_when(year == 2001 ~ "0102", 
-                                 year == 2002 ~ "0203",
-                                 year == 2003 ~ "0304",
-                                 year == 2004 ~ "0405",
-                                 year == 2005 ~ "0506",
-                                 year == 2006 ~ "0607",
-                                 year == 2007 ~ "0708",
-                                 year == 2008 ~ "0809",
-                                 year == 2009 ~ "0910",
-                                 year == 2010 ~ "1011",
-                                 year == 2011 ~ "1112",
-                                 year == 2012 ~ "1213",
-                                 year == 2013 ~ "1314",
-                                 year == 2014 ~ "1415",
-                                 year == 2015 ~ "1516",
-                                 year == 2016 ~ "1617",
-                                 year == 2017 ~ "1718",
-                                 year == 2018 ~ "1819"))  
+hb_pop_estimates <- apply_pop_est_filter(hb_pop_estimates)
 
 # call the function for creating HB cypher
 hb_pop_estimates <- apply_hb2019_cypher(hb_pop_estimates) 
@@ -701,27 +681,8 @@ ca_pop_estimates <- readRDS(paste0(
   lookup_folder, "/Unicode/Populations/Estimates/CA2019_pop_est_1981_2018.rds")) %>%
   rename(year = Year, age = Age, pop = Pop)
 
-ca_pop_estimates <- ca_pop_estimates %>% 
-  filter(age == 5) %>%
-  filter(year >= 2001 & year <=2018) %>% 
-  mutate(schlyr_exam = case_when(year == 2001 ~ "0102", 
-                                 year == 2002 ~ "0203",
-                                 year == 2003 ~ "0304",
-                                 year == 2004 ~ "0405",
-                                 year == 2005 ~ "0506",
-                                 year == 2006 ~ "0607",
-                                 year == 2007 ~ "0708",
-                                 year == 2008 ~ "0809",
-                                 year == 2009 ~ "0910",
-                                 year == 2010 ~ "1011",
-                                 year == 2011 ~ "1112",
-                                 year == 2012 ~ "1213",
-                                 year == 2013 ~ "1314",
-                                 year == 2014 ~ "1415",
-                                 year == 2015 ~ "1516",
-                                 year == 2016 ~ "1617",
-                                 year == 2017 ~ "1718",
-                                 year == 2018 ~ "1819"))  
+ca_pop_estimates <- apply_pop_est_filter(ca_pop_estimates)
+
 
 # create totals for individual council areas (for ca_pop_estimates)
 # council area level
@@ -911,27 +872,7 @@ sco_pop_estimates <- readRDS(paste0(
   lookup_folder, "/Unicode/Populations/Estimates/HB2019_pop_est_1981_2018.rds")) %>%
   rename(year = Year, age = Age, pop = Pop)
 
-sco_pop_estimates <- sco_pop_estimates %>% 
-  filter(age == 5) %>%
-  filter(year >= 2001 & year <=2018) %>% 
-  mutate(schlyr_exam = case_when(year == 2001 ~ "0102", 
-                                 year == 2002 ~ "0203",
-                                 year == 2003 ~ "0304",
-                                 year == 2004 ~ "0405",
-                                 year == 2005 ~ "0506",
-                                 year == 2006 ~ "0607",
-                                 year == 2007 ~ "0708",
-                                 year == 2008 ~ "0809",
-                                 year == 2009 ~ "0910",
-                                 year == 2010 ~ "1011",
-                                 year == 2011 ~ "1112",
-                                 year == 2012 ~ "1213",
-                                 year == 2013 ~ "1314",
-                                 year == 2014 ~ "1415",
-                                 year == 2015 ~ "1516",
-                                 year == 2016 ~ "1617",
-                                 year == 2017 ~ "1718",
-                                 year == 2018 ~ "1819"))  
+sco_pop_estimates <- apply_pop_est_filter(sco_pop_estimates)
 
 # Scotland level population estimates by year
 sco_pop_estimates <- rbind(sco_pop_estimates %>% 
